096a7c5e6569652efeacc07f4268d14d6eb83fd6
olivierh65 <ol65@sfr.fr>
2026-01-12 18:36:11 +0100

    first prepare media_centric

--- src/Traits/ProcessAlbumTrait.php
+++ src/Traits/ProcessAlbumTrait.php
@@ -5,8 +5,6 @@ namespace Drupal\lightgallery_views_flex_justified\Traits;
 use Drupal\file\FileInterface;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\media\MediaInterface;
-use Drupal\Core\Url;
-use Drupal\views\ViewExecutable;
 use Drupal\image\Entity\ImageStyle;
 
 /**
@@ -52,17 +50,18 @@ trait ProcessAlbumTrait {
    * @return array
    *   Normalized structure for Twig.
    */
-  private function processGroupRecursive(array $groups, array &$build, array &$lightgallery_settings, int $depth = 0) {
+  private function processGroupRecursive(array $groups, array &$build, array &$lightgallery_settings, int $depth = 0, $idx = 0) {
 
     $processed = [];
 
     foreach ($groups as $group_key => $group_data) {
+      $idx = rand();
       $group_item = [
         'title' => $group_data['group'] ?? '',
         'level' => $group_data['level'] ?? $depth,
         'albums' => [],
         'subgroups' => [],
-        'groupid' => 'album-group-' . rand(),
+        'groupid' => 'album-group-' . $idx,
       ];
 
       // Check if this group contains rows (final results)
@@ -78,22 +77,114 @@ trait ProcessAlbumTrait {
           $build,
           $lightgallery_settings,
           $depth + 1,
+          $idx
           );
         }
         else {
-          // These are real rows (ResultRow), process albums.
-          foreach ($group_data['rows'] as $index => $row) {
-            $album_data = $this->buildAlbumData($row, $index, $lightgallery_settings);
-            if ($album_data) {
-              $group_item['albums'][] = $album_data;
-            }
+          $r = $this->buildAlbumDataFromGroup($group_data['rows'], $idx, $lightgallery_settings);
+          if ($r) {
+            $group_item['albums'][] = $r;
           }
+          /* // These are real rows (ResultRow), process albums.
+          foreach ($group_data['rows'] as $index => $row) {
+          $album_data = $this->buildAlbumData($row, $index, $lightgallery_settings);
+          if ($album_data) {
+          $group_item['albums'][] = $album_data;
+          }
+          } */
         }
       }
       $processed[] = $group_item;
     }
 
     return $processed;
+  }
+
+  /**
+   * Build album data from grouped rows.
+   *
+   * @param array $rows
+   *   Array of rows in the same group.
+   * @param int $group_index
+   *   The group index.
+   * @param array $lightgallery_album_settings
+   *   The lightgallery settings (passed by reference to collect).
+   *
+   * @return array|null
+   *   The album data or NULL on error.
+   */
+  private function buildAlbumDataFromGroup($rows, $group_index, array &$lightgallery_album_settings) {
+    $medias = [];
+    $first_media = NULL;
+    $title = '';
+    $author = '';
+    $description = '';
+
+    foreach ($rows as $index => $row) {
+      $this->view->row_index = $index;
+
+      // Get the media entity from the row.
+      if (!isset($row->_entity) || !$row->_entity instanceof MediaInterface) {
+        continue;
+      }
+
+      $media = $row->_entity;
+
+      // Keep first media for album thumbnail.
+      if (!$first_media) {
+        $first_media = $media;
+
+        // Get text fields from first row.
+        $title = !empty($this->options['image']['title_field'])
+        ? $this->getFieldValue($index, $this->options['image']['title_field'])
+        : '';
+        $author = !empty($this->options['image']['author_field'])
+        ? $this->getFieldValue($index, $this->options['image']['author_field'])
+        : '';
+        $description = !empty($this->options['image']['description_field'])
+        ? $this->getFieldValue($index, $this->options['image']['description_field'])
+        : '';
+      }
+
+      // Get source field and build media data.
+      $source_field = $this->getSourceField($media);
+      if (!$source_field) {
+        continue;
+      }
+
+      $media_data = $this->buildMediaItemData($media, $source_field);
+      if ($media_data) {
+        $medias[] = $media_data;
+      }
+    }
+
+    if (empty($medias) || !$first_media) {
+      return NULL;
+    }
+
+    // Use first media's thumbnail as album image.
+    $image_url = $medias[0]['thumbnail'] ?? $medias[0]['url'] ?? '';
+
+    // Get lightgallery settings.
+    $lightgallery_settings = $this->getLightgallerySettings($first_media);
+
+    // Build plugins list and attach libraries.
+    foreach ($lightgallery_settings['plugins'] ?? [] as $plugin_name => $plugin) {
+      $lightgallery_album_settings['plugins'][$plugin_name] = 'lightgallery/lightgallery-' . $plugin_name ?? $plugin_name;
+    }
+
+    $album_id = 'album-group-' . $group_index;
+    $lightgallery_album_settings[$album_id] = $lightgallery_settings;
+
+    return [
+      'id' => $album_id,
+      'image_url' => $image_url,
+      'title' => $title,
+      'author' => $author,
+      'description' => $description,
+      'url' => '',
+      'medias' => $medias,
+    ];
   }
 
   /**
@@ -112,10 +203,25 @@ trait ProcessAlbumTrait {
   private function buildAlbumData($row, $index, array &$lightgallery_album_settings) {
     $this->view->row_index = $index;
 
-    // Get first media as presentation image.
-    $image_url = $this->getMediaImageUrl($row, $this->options['image']['image_field']);
+    // Get the media entity from the row.
+    $media = NULL;
+    if (isset($row->_entity) && $row->_entity instanceof MediaInterface) {
+      $media = $row->_entity;
+    }
 
-    // Text fields.
+    if (!$media) {
+      \Drupal::logger('album_gallery')->warning('No media entity found in row @index', ['@index' => $index]);
+      return NULL;
+    }
+
+    // Get source field for this media.
+    $source_field = $this->getSourceField($media);
+    if (!$source_field) {
+      \Drupal::logger('album_gallery')->warning('No source field found for media @id', ['@id' => $media->id()]);
+      return NULL;
+    }
+
+    // Text fields from the view row.
     $title = !empty($this->options['image']['title_field'])
     ? $this->getFieldValue($index, $this->options['image']['title_field'])
     : '';
@@ -125,122 +231,151 @@ trait ProcessAlbumTrait {
     $description = !empty($this->options['image']['description_field'])
     ? $this->getFieldValue($index, $this->options['image']['description_field'])
     : '';
-    $url = Url::fromRoute('entity.node.canonical', ['node' => $row->nid])->toString();
 
-    // Get all media items associated with this album.
-    $medias = [];
-    if (isset($row->_entity)
-      && $row->_entity instanceof EntityInterface
-      && $row->_entity->hasField($this->options['image']['image_field'])
-    ) {
-      foreach ($row->_entity->get($this->options['image']['image_field']) as $media_item) {
-        $media = $media_item->entity;
+    // Build media item data based on type.
+    $media_data = $this->buildMediaItemData($media, $source_field);
 
-        if (!$media instanceof MediaInterface) {
-          continue;
-        }
-
-        $source_field = $this->getSourceField($media);
-        if (!$source_field) {
-          continue;
-        }
-
-        switch ($media->getSource()->getPluginId()) {
-          case 'image':
-            $file = $media->get($source_field)->entity;
-            if ($file instanceof FileInterface) {
-              $original_url = $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri());
-              $thumbnail_url = $original_url;
-
-              if (!empty($this->options['image']['image_thumbnail_style'])) {
-                try {
-                  $image_style = ImageStyle::load($this->options['image']['image_thumbnail_style']);
-                  if ($image_style) {
-                    $thumbnail_url = $image_style->buildUrl($file->getFileUri());
-                  }
-                }
-                catch (\Exception $e) {
-                  \Drupal::logger('album_gallery')->error('Error loading image style: @error',
-                  ['@error' => $e->getMessage()]);
-                }
-              }
-
-              $medias[] = [
-                'url' => $original_url,
-                'mime_type' => $file->getMimeType(),
-                'alt' => $media->get($source_field)->first()->get('alt')->getValue() ?? '',
-                'title' => $media->get($source_field)->first()->get('title')->getValue() ?? '',
-                'thumbnail' => $thumbnail_url,
-              ];
-            }
-            break;
-
-          case 'video_file':
-            $file = $media->get($source_field)->entity;
-            $thumbnail = $media->get('thumbnail')->entity;
-            if ($file instanceof FileInterface) {
-              $thumbnail_url = '';
-              if ($thumbnail) {
-                $thumbnail_url = $this->fileUrlGenerator->generateAbsoluteString($thumbnail->getFileUri());
-
-                if (!empty($this->options['image']['image_thumbnail_style'])) {
-                  try {
-                    $image_style = ImageStyle::load($this->options['image']['image_thumbnail_style']);
-                    if ($image_style) {
-                      $thumbnail_url = $image_style->buildUrl($thumbnail->getFileUri());
-                    }
-                  }
-                  catch (\Exception $e) {
-                    \Drupal::logger('album_gallery')->error('Error loading image style for video thumbnail: @error',
-                    ['@error' => $e->getMessage()]);
-                  }
-                }
-              }
-
-              $medias[] = [
-                'url' => $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri()),
-                'mime_type' => $file->getMimeType(),
-                'thumbnail' => $thumbnail_url,
-                'title' => $media->get($source_field)->first()->get('description')->getValue() ?? '',
-              ];
-            }
-            break;
-        }
-      }
-    }
-    else {
-      \Drupal::logger('album_gallery')->warning('Row entity is missing or does not have the expected field.');
+    if (!$media_data) {
+      \Drupal::logger('album_gallery')->warning('Failed to build media data for media @id', ['@id' => $media->id()]);
+      return NULL;
     }
 
-    // Get lightgallery settings.
-    $renderer = $this->view->display_handler->getOption('fields')[$this->options['image']['image_field']]['settings']['view_mode'] ?? 'default';
-    $display = \Drupal::service('entity_display.repository')->getViewDisplay('node', $row->_entity->bundle(), $renderer);
+    // Use the first media item's thumbnail as the album image.
+    $image_url = $media_data['thumbnail'] ?? $media_data['url'] ?? '';
 
-    $node_settings = [];
-    if ($display && $display->getComponent($this->options['image']['image_field'])) {
-      $node_settings = $display->getComponent($this->options['image']['image_field'])['settings'];
-    }
+    // Get lightgallery settings from the unified media field view mode.
+    $lightgallery_settings = $this->getLightgallerySettings($media);
 
-    // Build the settings for the album.
-    // Same settings for all albums, as they use the same formatter/renderer,
-    // but Views can return different content types.
-    $lightgallery_settings = self::getGeneralSettings($node_settings);
     // Build plugins list and attach libraries.
     foreach ($lightgallery_settings['plugins'] ?? [] as $plugin_name => $plugin) {
       $lightgallery_album_settings['plugins'][$plugin_name] = 'lightgallery/lightgallery-' . $plugin_name ?? $plugin_name;
     }
 
-    $album_id = 'album-item-' . rand();
+    $album_id = 'album-item-' . $media->id() . '-' . $index;
     $lightgallery_album_settings[$album_id] = $lightgallery_settings;
+
     return [
       'id' => $album_id,
       'image_url' => $image_url,
       'title' => $title,
       'author' => $author,
       'description' => $description,
-      'url' => $url,
-      'medias' => $medias,
+    // Pas d'URL de node dans ce contexte.
+      'url' => '',
+    // Un seul média par row.
+      'medias' => [$media_data],
     ];
+  }
+
+  /**
+   * Build media item data for a single media entity.
+   *
+   * @param \Drupal\media\MediaInterface $media
+   *   The media entity.
+   * @param string $source_field
+   *   The source field name.
+   *
+   * @return array|null
+   *   The media item data or NULL on error.
+   */
+  private function buildMediaItemData(MediaInterface $media, $source_field) {
+    switch ($media->getSource()->getPluginId()) {
+      case 'image':
+        $file = $media->get($source_field)->entity;
+        if ($file instanceof FileInterface) {
+          $original_url = $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri());
+          $thumbnail_url = $original_url;
+
+          if (!empty($this->options['image']['image_thumbnail_style'])) {
+            try {
+              $image_style = ImageStyle::load($this->options['image']['image_thumbnail_style']);
+              if ($image_style) {
+                $thumbnail_url = $image_style->buildUrl($file->getFileUri());
+              }
+            }
+            catch (\Exception $e) {
+              \Drupal::logger('album_gallery')->error('Error loading image style: @error',
+              ['@error' => $e->getMessage()]);
+            }
+          }
+
+          return [
+            'url' => $original_url,
+            'mime_type' => $file->getMimeType(),
+            'alt' => $media->get($source_field)->first()->get('alt')->getValue() ?? '',
+            'title' => $media->get($source_field)->first()->get('title')->getValue() ?? '',
+            'thumbnail' => $thumbnail_url,
+          ];
+        }
+        break;
+
+      case 'video_file':
+        $file = $media->get($source_field)->entity;
+        $thumbnail = $media->get('thumbnail')->entity;
+        if ($file instanceof FileInterface) {
+          $thumbnail_url = '';
+          if ($thumbnail) {
+            $thumbnail_url = $this->fileUrlGenerator->generateAbsoluteString($thumbnail->getFileUri());
+
+            if (!empty($this->options['image']['image_thumbnail_style'])) {
+              try {
+                $image_style = ImageStyle::load($this->options['image']['image_thumbnail_style']);
+                if ($image_style) {
+                  $thumbnail_url = $image_style->buildUrl($thumbnail->getFileUri());
+                }
+              }
+              catch (\Exception $e) {
+                \Drupal::logger('album_gallery')->error('Error loading image style for video thumbnail: @error',
+                ['@error' => $e->getMessage()]);
+              }
+            }
+          }
+
+          return [
+            'url' => $this->fileUrlGenerator->generateAbsoluteString($file->getFileUri()),
+            'mime_type' => $file->getMimeType(),
+            'thumbnail' => $thumbnail_url,
+            'title' => $media->get($source_field)->first()->get('description')->getValue() ?? '',
+          ];
+        }
+        break;
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Get lightgallery settings for a media entity.
+   *
+   * @param \Drupal\media\MediaInterface $media
+   *   The media entity.
+   *
+   * @return array
+   *   The lightgallery settings.
+   */
+  private function getLightgallerySettings(MediaInterface $media) {
+    // Get view mode from the unified media field configuration.
+    $view_mode = 'default';
+    $fields = $this->view->display_handler->getOption('fields');
+
+    if (isset($fields['media_album_av_unified_media']['view_mode'])) {
+      $view_mode = $fields['media_album_av_unified_media']['view_mode'];
+    }
+
+    // Get display configuration.
+    $display = \Drupal::service('entity_display.repository')
+      ->getViewDisplay('media', $media->bundle(), $view_mode);
+
+    $node_settings = [];
+    if ($display) {
+      // Try to get settings from the source field.
+      $source_field = $this->getSourceField($media);
+      if ($source_field && $display->getComponent($source_field)) {
+        $node_settings = $display->getComponent($source_field)['settings'];
+      }
+    }
+
+    return self::getGeneralSettings($node_settings);
   }
 
   /**
@@ -435,6 +570,110 @@ trait ProcessAlbumTrait {
     }
 
     return [$text_fields, $media_fields, $taxo_fields];
+  }
+
+  /**
+   * Retreives text, taxonomy, media, and number fields from the view.
+   *
+   * @param bool $hidden
+   *   Whether to include hidden fields.
+   *   True to exclude hidden fields.
+   *
+   * @return array
+   *   An array containing arrays of text fields, media fields, taxonomy fields, and number
+   */
+  protected function getTextAndMediaFieldsCentric($hidden = FALSE) {
+    // Récupérer tous les champs de la vue.
+    $fields = $this->displayHandler->getHandlers('field');
+
+    // Préparer les options pour les champs texte.
+    $text_fields = [];
+    $taxonomy_fields = [];
+    $media_fields = [];
+    $number_fields = [];
+
+    foreach ($fields as $field_name => $field) {
+
+      $is_excluded = $hidden && !empty($field->options['exclude']);
+
+      if ($is_excluded) {
+        continue;
+      }
+
+      $label = $field->adminLabel() ?: $field_name;
+
+      // Identifier le type de champ.
+      $plugin_id = $field->getPluginId();
+      $field_definition = $field->definition ?? [];
+      $field_identifier = $field->field ?? NULL;
+
+      // Champs de base de l'entité media (name, created, etc.)
+      // On vérifie le nom du champ dans la table.
+      if (isset($field->table) && $field->table === 'media_field_data') {
+        // Champ name (titre du media)
+        if ($field_identifier === 'name') {
+          $text_fields[$field_name] = [
+            'label' => $label,
+            'excluded' => $is_excluded,
+          ];
+        }
+      }
+      // Champs texte.
+      if (in_array($plugin_id, ['field']) && isset($field->definition['field_name'])) {
+        $field_storage_config = \Drupal::entityTypeManager()
+          ->getStorage('field_storage_config')
+          ->load('media.' . $field->definition['field_name']);
+
+        if ($field_storage_config) {
+          $field_type = $field_storage_config->getType();
+
+          switch ($field_type) {
+            case 'string':
+            case 'string_long':
+            case 'text':
+            case 'text_long':
+            case 'text_with_summary':
+              $text_fields[$field_name] = $label;
+              break;
+
+            case 'entity_reference':
+              // Vérifier si c'est une taxonomie.
+              $settings = $field_storage_config->getSettings();
+              if (isset($settings['target_type']) && $settings['target_type'] === 'taxonomy_term') {
+                $taxonomy_fields[$field_name] = $label;
+              }
+              break;
+
+            case 'integer':
+            case 'decimal':
+            case 'float':
+              $number_fields[$field_name] = $label;
+              break;
+
+            case 'image':
+            case 'file':
+              $media_fields[$field_name] = $label;
+              break;
+          }
+        }
+      }
+
+      // Champs spéciaux (comme votre media_album_av_unified_media)
+      if ($plugin_id === 'media_album_av_unified_media') {
+        $media_fields[$field_name] = $label;
+      }
+
+      // Champs de type entity_reference_label (taxonomies)
+      if ($plugin_id === 'entity_reference_label') {
+        $taxonomy_fields[$field_name] = $label;
+      }
+
+      // Champs de type number.
+      if ($plugin_id === 'number_integer' || $plugin_id === 'number_decimal') {
+        $number_fields[$field_name] = $label;
+      }
+    }
+    return [$text_fields, $media_fields, $taxonomy_fields, $number_fields];
   }
 
   /**
